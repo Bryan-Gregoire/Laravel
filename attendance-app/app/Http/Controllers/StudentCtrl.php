46ee6d73da56156c3adcef0a866f8c19efb7b2d5
Jeremie Seshie <54627@etu.he2b.be>
2021-09-24 11:43:55 +0200

    implement the prototype view

---
+++ attendance-app/app/Http/Controllers/StudentCtrl.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+ 
+class StudentCtrt extends Controller{
+
+    public function index(){
+        return View('home');
+    }
+}
---
+++ attendance-app/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Titles</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href=css/css.css>
+</head>
+
+<header>
+    <h1> Gestion de Projet - gestions des etudiants </h1>
+</header>
+<body>
+<table>
+<tr>
+    <th>Matricule</th>
+    <th>Nom</th>
+    <th>Prénom</th>
+</tr>
+</table>
+</body>
+<footer>
+    <h1>Gestion de Projet</h1>
+</footer>
+
+</html>
