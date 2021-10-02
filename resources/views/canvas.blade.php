a2f5e06984e866ed6a6a2fa089d0c8a6f1855faa
Bryan Grégoire <53735@etu.he2b.be>
2021-10-02 16:06:59 +0200

    secure css for heroku

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 
